0b46fffbf853fdc5df24e6a4f366c137fad47869
Denilson <fcodenilson@gmail.com>
2025-03-16 17:37:13 -0300

    Remove commented-out method and set Stage to maximized in primary FXML

--- src/main/java/io/github/codenilson/smartpat/App.java
+++ src/main/java/io/github/codenilson/smartpat/App.java
@@ -19,10 +19,6 @@ public class App extends Application {
         stage.show();
     }
 
-    // public static void setRoot(String fxml) throws IOException {
-    // scene.setRoot(loadFXML(fxml));
-    // }
-
     private static Stage loadFXML(String fxml) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
         return fxmlLoader.load();
