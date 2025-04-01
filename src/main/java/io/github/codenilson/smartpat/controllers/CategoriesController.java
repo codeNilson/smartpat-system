6bc245804ad19d15fafab1bdae848a32bb34a818
Denilson <fcodenilson@gmail.com>
2025-03-31 21:15:56 -0300

    feat: enhance category image handling and container resizing logic

--- src/main/java/io/github/codenilson/smartpat/controllers/CategoriesController.java
+++ src/main/java/io/github/codenilson/smartpat/controllers/CategoriesController.java
@@ -1,6 +1,8 @@
 package io.github.codenilson.smartpat.controllers;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 import javafx.animation.ScaleTransition;
@@ -22,53 +24,67 @@ public class CategoriesController implements Initializable {
     @FXML
     private TilePane categoriesContainer;
 
+    private List<ImageView> categoriesImages = new ArrayList<>();
+
+    private static final double MIN_TILE_WIDTH = 200;
+    private static final double MIN_IMAGE_WIDTH = 150;
+    private static final double TILE_WIDTH_RATIO = 6;
+    private static final double IMAGE_WIDTH_RATIO = 7;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
-        applyScaleToCategories();
+        Platform.runLater(() -> {
 
-        adjustCategoryContainerSize();
+            loadCategoryImageViews();
+            applyScaleToCategories();
+            adjustCategoryContainerDimensions();
+        });
+    }
 
+    private void loadCategoryImageViews() {
+        for (Node node : root.lookupAll(".category-image")) {
+            if (node instanceof ImageView) {
+                categoriesImages.add((ImageView) node);
+            }
+        }
     }
 
     private void applyScaleToCategories() {
         for (Node node : root.lookupAll(".category")) {
             applyScaleAnimation(node);
+
         }
     }
 
-    private void adjustCategoryContainerSize() {
-        Platform.runLater(() -> {
-            Stage stage = (Stage) root.getScene().getWindow();
-            stage.widthProperty().addListener((obs, oldVal, newVal) -> {
+    private void adjustCategoryContainerDimensions() {
 
-                if (stage.isMaximized()) {
-                    double newWidth = newVal.doubleValue() / 6;
-                    categoriesContainer.setPrefTileWidth(newWidth);
-                    categoriesContainer.setPrefTileHeight(newWidth);
+        Stage stage = (Stage) root.getScene().getWindow();
 
-                    for (Node node : root.lookupAll(".category-image")) {
-                        if (node instanceof ImageView) {
-                            ImageView img = (ImageView) node;
-                            img.setFitWidth(newWidth / 1.2);
-                            img.setFitHeight(newWidth / 1.2);
-                        }
-                    }
-
-                } else {
-                    categoriesContainer.setPrefTileHeight(200);
-                    categoriesContainer.setPrefTileWidth(200);
-
-                    for (Node node : root.lookupAll(".category-image")) {
-                        if (node instanceof ImageView) {
-                            ImageView img = (ImageView) node;
-                            img.setFitWidth(150);
-                            img.setFitHeight(150);
-                        }
-                    }
-                }
+        stage.maximizedProperty().addListener((obs, oldVal, newVal) -> {
+            Platform.runLater(() -> {
+                adjustContainerSize(stage);
+                adjustImageSize(stage);
             });
         });
+    }
+
+    private void adjustImageSize(Stage stage) {
+        Double maxImageWidth = stage.getWidth() / IMAGE_WIDTH_RATIO;
+        Double newImageWidth = Math.max(MIN_IMAGE_WIDTH, maxImageWidth);
+
+        for (ImageView imageView : categoriesImages) {
+            imageView.setFitWidth(newImageWidth);
+            imageView.setFitHeight(newImageWidth);
+        }
+    }
+
+    private void adjustContainerSize(Stage stage) {
+        double maxWidth = stage.getWidth() / TILE_WIDTH_RATIO;
+        double newWidth = Math.max(MIN_TILE_WIDTH, maxWidth);
+
+        categoriesContainer.setPrefTileWidth(newWidth);
+        categoriesContainer.setPrefTileHeight(newWidth);
     }
 
     public static void applyScaleAnimation(Node node) {
